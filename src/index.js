d6146f50a8ef8a02045b2288f4ab8f8d3bb04859
AtlasShd <tlsshrugged@gmail.com>
2022-01-15 22:09:24 +0300

    feat: add solution of towel sort task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
+'use strict';
 
-// You should implement your task here.
-
-module.exports = function towelSort (matrix) {
-  return [];
-}
+module.exports = function towelSort(matrix) {
+    if (!matrix) return [];
+    let result = [];
+    let order = 0;
+    matrix.forEach(arr => {
+        if (++order % 2 == 1) {
+            for (let i = 0; i < arr.length; i++) {
+                result.push(arr[i]);
+            }
+        } else {
+            for (let i = arr.length - 1; i >= 0; i--) {
+                result.push(arr[i]);
+            }
+        }
+    });
+    return result;
+};
